eb7cf02eda74eb3b62bf16094a0ebf64a0a2c12f
Sokhna Anta Mbacke <SokhnaAntaMbacke@esp.sn>
2024-03-29 14:25:50 +0000

    first commit

---
+++ src/main/java/dgi/esp/ucad/SbCarApplication.java
@@ -0,0 +1,38 @@
+package dgi.esp.ucad;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value ;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCarApplication{
+	private static final Logger logger = LoggerFactory.getLogger(SbCarApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SbCarApplication.class, args);
+		logger.info("Car App started ! Goto http://localhost:8081/dic_student_name");
+	}
+
+}
+
+@RestController
+@RequestMapping("/dic_student_name")
+class SbCarController{
+	@Value("${fullname: SAM}")
+	private String fullname ;
+	@GetMapping
+	String getFullname(){
+		return "work done by "+this.fullname+"!" ;
+	}
+
+
+	
+}
